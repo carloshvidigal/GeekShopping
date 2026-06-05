d7df8d2dd22461f59c400fcc0c3c4d63ed2452ec
chmov <chmov@PC-CARLOSHMV>
2026-06-05 14:51:16 -0300

    feat: mappingConfig

---
+++ GeekShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+
+namespace GeekShopping.ProductAPI.Config;
+
+public class MappingConfig : Profile
+{
+    public MappingConfig()
+    {
+        CreateMap<ProductVO, Product>();
+        CreateMap<Product, ProductVO>();
+    }
+}
