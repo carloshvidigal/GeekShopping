2772e3868b3a721da3a808a7d52948c155176797
chmov <chmov@PC-CARLOSHMV>
2026-08-02 16:52:54 -0300

    feat: add DetailsPost method

--- GeekShopping.Web/Controllers/HomeController.cs
+++ GeekShopping.Web/Controllers/HomeController.cs
@@ -11,12 +11,14 @@ namespace GeekShopping.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IProductService _productService;
+        private readonly ICartService _cartService;
 
 
-        public HomeController(ILogger<HomeController> logger, IProductService productService)
+        public HomeController(ILogger<HomeController> logger, IProductService productService, ICartService cartService)
         {
             _logger = logger;
             _productService = productService;
+            _cartService = cartService;
         }
 
         public async Task<IActionResult> Index()
@@ -33,39 +35,39 @@ namespace GeekShopping.Web.Controllers
             return View(model);
         }
 
-        //[HttpPost]
-        //[ActionName("Details")]
-        //[Authorize]
-        //public async Task<IActionResult> DetailsPost(ProductViewModel model)
-        //{
-        //    var token = await HttpContext.GetTokenAsync("access_token");
+        [HttpPost]
+        [ActionName("Details")]
+        [Authorize]
+        public async Task<IActionResult> DetailsPost(ProductViewModel model)
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
 
-        //    CartViewModel cart = new()
-        //    {
-        //        CartHeader = new CartHeaderViewModel
-        //        {
-        //            UserId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value
-        //        }
-        //    };
+            CartViewModel cart = new()
+            {
+                CartHeader = new CartHeaderViewModel
+                {
+                    UserId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value
+                }
+            };
 
-        //    CartDetailViewModel cartDetail = new CartDetailViewModel()
-        //    {
-        //        Count = model.Count,
-        //        ProductId = model.Id,
-        //        Product = await _productService.FindProductById(model.Id, token)
-        //    };
+            CartDetailViewModel cartDetail = new CartDetailViewModel()
+            {
+                Count = model.Count,
+                ProductId = model.Id,
+                Product = await _productService.FindProductById(model.Id, token)
+            };
 
-        //    List<CartDetailViewModel> cartDetails = new List<CartDetailViewModel>();
-        //    cartDetails.Add(cartDetail);
-        //    cart.CartDetails = cartDetails;
+            List<CartDetailViewModel> cartDetails = new List<CartDetailViewModel>();
+            cartDetails.Add(cartDetail);
+            cart.CartDetails = cartDetails;
 
-        //    var response = await _cartService.AddItemToCart(cart, token);
-        //    if (response != null)
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(model);
-        //}
+            var response = await _cartService.AddItemToCart(cart, token);
+            if (response != null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
 
         public IActionResult Privacy()
         {
