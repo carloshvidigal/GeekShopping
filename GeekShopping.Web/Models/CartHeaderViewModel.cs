2e0ed71a363b89a908bbb13f55c547e3d73386e1
chmov <chmov@PC-CARLOSHMV>
2026-08-18 11:52:08 -0300

    feat: add parameter DiscountAmount

--- GeekShopping.Web/Models/CartHeaderViewModel.cs
+++ GeekShopping.Web/Models/CartHeaderViewModel.cs
@@ -6,5 +6,7 @@
         public string UserId { get; set; }
         public string CouponCode { get; set; }
         public decimal PurchaseAmount { get; set; }
+
+        public decimal DiscountAmount { get; set; }
     }
 }
