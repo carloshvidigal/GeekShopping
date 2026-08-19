aa7f45eb3fea4d3d6ca541019691690be7542be9
chmov <chmov@PC-CARLOSHMV>
2026-08-19 10:44:55 -0300

    feat: implement checkout

--- GeekShopping.CartAPI/Messages/CheckoutHeaderVO.cs
+++ GeekShopping.CartAPI/Messages/CheckoutHeaderVO.cs
@@ -1,12 +1,16 @@
-﻿namespace GeekShopping.Web.Models
-{
-    public class CartHeaderViewModel
-    {
-        public long Id { get; set; }
-        public string UserId { get; set; }
-        public string CouponCode { get; set; }
-        public decimal PurchaseAmount { get; set; }
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace GeekShopping.CartAPI.Messages
+{
+    public class CheckoutHeaderVO 
+    {
+        public string UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public decimal PurchaseAmount { get; set; }
         public decimal DiscountAmount { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -16,5 +20,8 @@
         public string CardNumber { get; set; }
         public string CVV { get; set; }
         public string ExpiryMothYear { get; set; }
+
+        public int CartTotalItens { get; set; }
+        public IEnumerable<CartDetailVO>? CartDetails { get; set; }
     }
 }
