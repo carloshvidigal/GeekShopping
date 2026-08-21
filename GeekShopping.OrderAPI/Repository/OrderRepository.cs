4c5f4bb6f5342197a119f47a0b09391587d0a7e1
chmov <chmov@PC-CARLOSHMV>
2026-08-21 10:32:52 -0300

    feat: add repository OrderAPI

---
+++ GeekShopping.OrderAPI/Repository/OrderRepository.cs
@@ -0,0 +1,35 @@
+﻿using GeekShopping.OrderAPI.Model;
+using GeekShopping.OrderAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace GeekShopping.OrderAPI.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly MySQLContext _context;
+
+        public OrderRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddOrder(OrderHeader header)
+        {
+            if (header == null) return false;
+            _context.Headers.Add(header);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task UpdateOrderPaymentStatus(long orderHeaderId, bool status)
+        {
+            var header = await _context.Headers.FirstOrDefaultAsync(o => o.Id == orderHeaderId);
+            if (header != null)
+            {
+                header.PaymentStatus = status;
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
