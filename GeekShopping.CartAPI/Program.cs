351d852edafeff35a1d84601d6565e478b6c7d99
chmov <chmov@PC-CARLOSHMV>
2026-09-10 16:03:44 -0300

    fix: register ICouponRepository in CartAPI DI container

--- GeekShopping.CartAPI/Program.cs
+++ GeekShopping.CartAPI/Program.cs
@@ -26,6 +26,11 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<ICartRepository, CartRepository>();
 
+builder.Services.AddHttpClient<ICouponRepository, CouponRepository>(c =>
+{
+    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"]!);
+});
+
 builder.Services.AddSingleton<IRabbitMQMessageSender, RabbitMQMessageSender>();
 
 builder.Services.AddControllers();
