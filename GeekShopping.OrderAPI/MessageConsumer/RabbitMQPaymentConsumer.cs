a88124bbc810e6816cb1b0b2ba23215f7beba0f6
chmov <chmov@PC-CARLOSHMV>
2026-09-10 16:19:19 -0300

    feat: create RabbitMQPaymentConsumer - OrderAPI

---
+++ GeekShopping.OrderAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
@@ -0,0 +1,61 @@
+﻿using GeekShopping.OrderAPI.Messages;
+using GeekShopping.OrderAPI.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace GeekShopping.OrderAPI.MessageConsumer
+{
+    public class RabbitMQPaymentConsumer : BackgroundService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMQPaymentConsumer(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "orderpaymentresultqueue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (chanel, evt) =>
+            {
+                var content = Encoding.UTF8.GetString(evt.Body.ToArray());
+                UpdatePaymentResultVO vo = JsonSerializer.Deserialize<UpdatePaymentResultVO>(content);
+                UpdatePaymentStatus(vo).GetAwaiter().GetResult();
+                _channel.BasicAck(evt.DeliveryTag, false);
+            };
+            _channel.BasicConsume("orderpaymentresultqueue", false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private async Task UpdatePaymentStatus(UpdatePaymentResultVO vo)
+        {
+            try
+            {
+                using var scope = _scopeFactory.CreateScope();
+                var repository = scope.ServiceProvider.GetRequiredService<IOrderRepository>();
+                await repository.UpdateOrderPaymentStatus(vo.OrderId, vo.Status);
+            }
+            catch (Exception)
+            {
+                //Log
+                throw;
+            }
+        }
+    }
+}
